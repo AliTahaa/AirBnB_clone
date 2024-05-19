5afd8089108f930598d350f5fd0ffbe7f39f01e8
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-05-19 19:02:18 +0300

    tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,38 +1,84 @@
 #!/usr/bin/python3
-"""
-Test city class
-"""
+""" Contains unit tests for class BaseModel """
 
 
 import unittest
-from models.city import City
+import io
+import sys
+from datetime import datetime
+from models.base_model import BaseModel
+import models
 
 
-class TestCity(unittest.TestCase):
-    """ test for city
+class TestBaseModel(unittest.TestCase):
+    """test for class BaseModel and its methods
     """
     def setUp(self):
-        """ standard setUp()
+        """ Set up method
         """
-        self.model = City()
+        self.base1 = BaseModel()
+        self.base2 = BaseModel()
 
-    def test_public_attr(self):
-        """ if public attribute exists and if equal to empty string
+    def tearDown(self):
+        """ Tear down method
         """
-        self.assertTrue(hasattr(self.model, "state_id"))
-        self.assertTrue(hasattr(self.model, "name"))
-        self.assertEqual(self.model.state_id, "")
-        self.assertEqual(self.model.name, "")
+        pass
 
-    def test_strings(self):
-        """ input for each attr
+    def test_docstring(self):
+        """ test doc in the file
         """
-        self.model.name = "San Francisco"
-        # OJO
-        self.model.state_id = "7d78d8de-a37f-4edd-9443-1578032a1eea"
-        self.assertEqual(self.model.name, "San Francisco")
-        self.assertEqual(self.model.state_id,
-                         "7d78d8de-a37f-4edd-9443-1578032a1eea")
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+
+    def test_uuid(self):
+        """ Test for uuid
+        """
+        self.assertNotEqual(self.base1.id, self.base2.id)
+        self.assertTrue(hasattr(self.base1, "id"))
+        self.assertEqual(type(self.base1.id), str)
+        self.assertEqual(type(self.base2.id), str)
+
+    def test_instance(self):
+        """ Test for instance
+        """
+        self.assertTrue(isinstance(self.base1, BaseModel))
+        self.assertTrue(isinstance(self.base2, BaseModel))
+
+    def test_type(self):
+        """ Test for type of object
+        """
+        self.assertEqual(type(self.base1), BaseModel)
+        self.assertEqual(type(self.base2), BaseModel)
+
+    def test_save(self):
+        """ Test for save
+        """
+        time = self.base1.updated_at
+        self.base1.save()
+        self.assertFalse(time == self.base1.updated_at)
+
+    def test_update(self):
+        """ Test for update
+        """
+        self.base1.name = "Holberton"
+        self.assertTrue(hasattr(self.base1, "name"))
+
+    def test_init_with_kwargs(self):
+        """ Test to init for kwargs
+        """
+        self.base1.name = "Holberton"
+        model_json = self.base1.to_dict()
+        new_model = BaseModel(**model_json)
+        self.assertDictEqual(model_json, new_model.to_dict())
+        self.assertIn("name", new_model.to_dict())
+        self.assertIsNot(self.base1, new_model)
+
+    def test_storage(self):
+        """ Test to staorage
+        """
+        obj_dict = models.storage.all()
+        self.assertTrue(obj_dict)
 
 
 if __name__ == '__main__':
