335dc0a15aca566e5d7d86084df6fb2e46b4130c
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-07-20 18:24:54 +0300

    fix task 4

--- models/base_model.py
+++ models/base_model.py
@@ -8,18 +8,22 @@ from datetime import datetime
 class BaseModel():
     """BaseModel class"""
     def __init__(self, *args, **kwargs):
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if kwargs != None:
+        if kwargs is not None:
             for key, value in kwargs.items():
-                self.__dict__[key] = value
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, t_format)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         class_name = self.__class__.__name__
         id = self.id
         data = self.__dict__
-        return"[{}] ({}) {}".format(class_name, id, data)
+        return "[{}] ({}) {}".format(class_name, id, data)
 
     def save(self):
         self.updated_at = datetime.now()
@@ -30,5 +34,3 @@ class BaseModel():
         data["created_at"] = datetime.now().isoformat()
         data["updated_at"] = datetime.now().isoformat()
         return data
-
-print(BaseModel().to_dict())
