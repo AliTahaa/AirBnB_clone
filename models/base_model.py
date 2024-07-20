b3328dbe08a0eb275b22e625f9e704bf8db52bc4
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-07-20 12:34:34 +0300

    try fix time

--- models/base_model.py
+++ models/base_model.py
@@ -9,8 +9,8 @@ class BaseModel():
     '''Basemodel class'''
     def __init__(self):
         self.id = str(uuid4())
-        self.created_at = str(datetime.now().isoformat())
-        self.updated_at = str(datetime.now().isoformat())
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
 
     def __str__(self):
         class_name = self.__class__.__name__
@@ -24,6 +24,6 @@ class BaseModel():
     def to_dict(self):
         data = self.__dict__.copy()
         data["__class__"] = self.__class__.__name__
-        data["created_at"] = str(datetime.now().isoformat())
-        data["updated_at"] = str(datetime.now().isoformat())
+        data["created_at"] = datetime.now().isoformat()
+        data["updated_at"] = datetime.now().isoformat()
         return data
