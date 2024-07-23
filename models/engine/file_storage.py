544faf8de681a342ff87791e0e2be141da8861b8
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-07-23 03:49:31 +0300

    back

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,16 +15,16 @@ class FileStorage():
         FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
+        objects = FileStorage.__objects
+        obj_dict = {}
+        for obj in objects.keys():
+            obj_dict[obj] = objects[obj].to_dict()
         with open(FileStorage.__file_path, "w") as file:
-            json.dump(FileStorage.__objects, file)
+            json.dump(obj_dict, file)
 
     def reload(self):
         if os.path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, "r") as file:
                 obj_dict = json.load(file)
-                for i in obj_dict.values():
-                    class_name = i["__class__"]
-                    del i["__class__"]
-                    self.new(eval(class_name)(**i))
         else:
             pass
