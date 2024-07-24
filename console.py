5decaea84d63c2c9e4ecd0281a9d835776536f2b
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-07-24 05:32:22 +0300

    Update console.py with new commands and functionality

--- console.py
+++ console.py
@@ -22,6 +22,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
+        '''Creates a new instance of BaseModel'''
         if not arg:
             print("** class name missing **")
         elif arg != "BaseModel":
@@ -32,6 +33,7 @@ class HBNBCommand(cmd.Cmd):
             print(model.id)
 
     def do_show(self, arg):
+        """Prints the string representation of an instance"""
         args = arg.split()
         if not arg:
             print("** class name missing **")
@@ -45,6 +47,72 @@ class HBNBCommand(cmd.Cmd):
                 print(storage.all()[key])
             else:
                 print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(args[0], args[1])
+            if key in storage.all():
+                del storage.all()[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances."""
+        commands = arg.split()
+        obj_dict = storage.all()
+        if len(commands) == 0:
+            for key, value in obj_dict.items():
+                print(str(value))
+        elif commands[0] != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            for key, value in obj_dict.items():
+                if key.split(".")[0] == commands[0]:
+                    print(str(value))
+
+    def do_update(self, arg):
+        """Updates an instance."""
+        commands = arg.split()
+        obj_dict = storage.all()
+
+        if len(commands) == 0:
+            print("** class name missing **")
+            return False
+        elif commands[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return False
+        elif len(commands) == 1:
+            print("** instance id missing **")
+            return False
+        elif "{}.{}".format(commands[0], commands[1]) not in obj_dict.keys():
+            print("** no instance found **")
+            return False
+        elif len(commands) == 2:
+            print("** attribute name missing **")
+            return False
+        elif len(commands) == 3:
+            print("** value missing **")
+            return False
+
+        else:
+            obj = obj_dict["{}.{}".format(commands[0], commands[1])]
+            att_name = commands[2]
+            att_value = commands[3]
+            try:
+                attr_value = eval(attr_value)
+            except Exception:
+                pass
+            setattr(obj, att_name, att_value)
+            storage.save()
 
 
 if __name__ == '__main__':
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ import json
 import os
 from models.base_model import BaseModel
 
+
 class FileStorage():
     __file_path = "file.json"
     __objects = {}
